05f5fa1a3f25f2608c347f01b1ea839fca685158
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2022-01-29 11:02:49 +0530

    Modified skeleton to push main.py to sourceCode Folder

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from sourceCode.randomFunction import printLog
 from sourceCode.constants import listOfCommands
 
 import argparse
@@ -12,11 +11,12 @@ import zlib
 
 argparser = argparse.ArgumentParser(description='Content Tracker')
 argSubParsers = argparser.add_subparsers(title="Commands", dest='command')
-argSubParsers.required = True
+argSubParsers.required = False
 
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
 
+    print("Passed this line")
     if args.command in listOfCommands:
         commandName = "cmd_" + args.command
         locals()[commandName]()
