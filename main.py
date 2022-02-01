65c68ea906a9f6efed96664b4f90c543b9b3a8cd
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2022-02-01 10:46:08 +0530

    git init command implemented. Can initialize a new git repository (albeit only in empty directories )

--- main.py
+++ main.py
@@ -3,32 +3,33 @@ from sourceCode.gitInit import cmd_init
 
 import argparse
 import collections
-import configparser
 import hashlib
-import os
 import re
 import sys
 import zlib
 
-argparser = argparse.ArgumentParser(description='Command Tracker')
-argSubParsers = argparser.add_subparsers(title='Commands', dest='command')
-argSubParsers.required = True
+def configArgParser():
+    argparser = argparse.ArgumentParser(description='Command Tracker')
+    argSubParsers = argparser.add_subparsers(title='Commands', dest='command')
+    argSubParsers.required = True
 
-argsp = argSubParsers.add_parser("init", help="Initialize a new empty git repository")
-argsp.add_argument("path",
-                    metavar="directory", 
-                    nargs="?", 
-                    default = ".", 
-                    help= "Where to create the git repository. Current directory by default")
+    # INIT COMMAND
+    argsp = argSubParsers.add_parser("init", help="Initialize a new empty git repository")
+    argsp.add_argument("path",
+                        metavar="directory", 
+                        nargs="?", 
+                        default = ".", 
+                        help= "Where to create the git repository. Current directory by default")
 
-def main(argv=sys.argv[1:]):
+    return argparser
+
+
+def main(argparser, argv=sys.argv[1:]):
     print(argv)
     args = argparser.parse_args(argv)
 
     if args.command in listOfCommands:
-        print("Found the command", args.command)
         commandName = "cmd_" + args.command
-        print("Calling", commandName)
         globals()[commandName](args)
     else:
         print("Unknown command: " + args.command)
--- sourceCode/gitInit.py
+++ sourceCode/gitInit.py
@@ -1,3 +1,47 @@
+import configparser
+import os
+import sys
+
+
+def repo_path(repo, *path):
+    ''' compute path under repo's gitdir '''
+    return os.path.join(repo.gitdir, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    '''Same as repo_path, but mkdir *path if absent if mkdir.'''
+    
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if(os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+    
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+def repo_file(repo, *path, mkdir=False):
+    ''' same as repo_path but creates a dirname(path) if absent.
+    For example , repo_file(r,  \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
+    .git/refs/remotes/origin.'''
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
+
 class gitRepository(object):
     ''' A git repository'''
 
@@ -14,11 +58,11 @@ class gitRepository(object):
         
         # Read config file in .git/config
         self.conf = configparser.ConfigParser()
-        cf = repo_file(self, 'config')
+        configFile = repo_file(self, 'config')
         
-        if cf and os.path.exists(cf):
-            self.conf.read([cf])
-        elif not cf:
+        if configFile and os.path.exists(configFile):
+            self.conf.read([configFile])
+        elif not force:
             raise Exception('Config file not found')
         
         if not force:
@@ -27,6 +71,44 @@ class gitRepository(object):
                 raise Exception("Unsupported repositoryformatversion %s" % vers)
 
 
+def repo_create(path):
+    ''' Creating a new git repository at path '''
+
+    repo = gitRepository(path, True)
+
+    # First , we shall make sure that the path either doesn't exist
+    # or is an empty directory.
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception("%s is not a directory" % path)
+        if os.listdir(repo.worktree):
+            raise Exception("%s is not empty!" % path)
+    else:
+        os.makedirs(repo.worktree)
+    
+    assert(repo_dir(repo, "branches", mkdir=True))
+    assert(repo_dir(repo, "objects", mkdir=True))
+    assert(repo_dir(repo, "refs", "tags", mkdir=True))
+    assert(repo_dir(repo, "regs", "heads", mkdir=True))
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+    
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+    
+    return repo
+
+
 def cmd_init(args):
     if args.path:
         print("Creating Git Repository at " + args.path)
+    try:
+        repo_create(args.path)
+    except Exception as e:
+        print(e)
+        print("Failed to create repository at " + args.path)
+        sys.exit(1)
